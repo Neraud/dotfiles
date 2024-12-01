4cd20eb43874c7b01995c23a93dde829e9512559
Neraud <github@a.neraud.fr>
2024-12-01 14:52:50 +0100

    Enable bashcompinit

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -1,5 +1,7 @@
 # Enable the "new" completion system (compsys).
 autoload -Uz compinit && compinit -d ${ZSH_CACHE_DIR}/zcompdump
+# Enable bashcompinit
+autoload -U +X bashcompinit && bashcompinit
 
 # Load completions
 for f (${ZSH_CONFIG_DIR}/completion.d/*.zsh(N)) source $f
