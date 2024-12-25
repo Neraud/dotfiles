da9eb2060ace379aca32fc667284f14c42cfcca3
Neraud <github@a.neraud.fr>
2024-12-25 18:00:10 +0100

    Add script to update versions in ansible variables

---
+++ ansible/update.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+DOTFILES_ROOT="${PROJECT_DIR}/.."
+
+function update_ansible_variable {
+    role=$1
+    variable=$2
+    new_value=$3
+
+    var_file="${PROJECT_DIR}/roles/${role}/vars/main.yml"
+
+    if [ ! -f "${var_file}" ] ; then
+        echo "Variable file '${var_file}' doesn't exist" >&2
+        return 1
+    fi
+
+    current_value=$(yq .${variable} ${var_file})
+
+    if [ "${current_value}" == "null" ] ; then
+        echo "Variable '${role}' : '${variable}' doesn't exist" >&2
+        return 1
+    fi
+
+    if [ "${new_value}" == "${current_value}" ] ; then
+        echo "${role} : ${variable} = ${current_value} : already up to date"
+    else
+        echo "${role} : ${variable} = ${current_value} => ${new_value}"
+        yq -i ".${variable} = \"${new_value}\"" ${var_file}
+    fi
+}
+
+echo "Updating ansible variables"
+eza_version=$(curl -s https://api.github.com/repos/eza-community/eza/releases | jq -r '.[0].name' | sed 's/^eza //')
+update_ansible_variable eza eza_version ${eza_version}
+
+fzf_version=$(curl -s https://api.github.com/repos/junegunn/fzf/releases | jq -r '.[0].name')
+update_ansible_variable fzf fzf_version ${fzf_version}
+
+k9s_version=$(curl -s https://api.github.com/repos/derailed/k9s/releases | jq -r '.[0].name')
+update_ansible_variable k9s k9s_version ${k9s_version}
+
+nerd_fonts_version=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases | jq -r '.[0].name')
+update_ansible_variable requirements nerd_fonts_version ${nerd_fonts_version}
+
+tpm_version=$(curl -s https://api.github.com/repos/tmux-plugins/tpm/tags | jq -r '.[0].name')
+update_ansible_variable tmux tpm_version ${tpm_version}
+
+yazi_version=$(curl -s https://api.github.com/repos/sxyazi/yazi/releases | jq -r '.[0].name')
+update_ansible_variable yazi yazi_version ${yazi_version}
+
+zoxide_version=$(curl -s https://api.github.com/repos/ajeetdsouza/zoxide/releases | jq -r '.[0].name')
+update_ansible_variable zoxide zoxide_version ${zoxide_version}
