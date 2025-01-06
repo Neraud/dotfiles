8fa9be17d9ad621872e3eec1f63f6f38042564fc
Neraud <github@a.neraud.fr>
2025-01-06 14:45:19 +0100

    feat: zsh: Disable PROMPT_EOL_MARK

--- zsh/.config/zsh/config.d/misc_options.zsh
+++ zsh/.config/zsh/config.d/misc_options.zsh
@@ -4,3 +4,8 @@
 setopt NO_GLOBAL_RCS
 # Treat the '#', '~' and '^' characters as part of patterns for filename generation, etc. (An initial unquoted '~' always produces named directory expansion.)
 setopt EXTENDED_GLOB
+
+# Hide the prompt after a partial line
+# Can be tested with: print -n "this is a test"
+# By default, it will print "this is a test%", and setting this to an empty string removes the trailing %
+export PROMPT_EOL_MARK=""
